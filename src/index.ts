de1a3f776da5117388e962695944e73a6d5c4bef
Davi Bastos <davi@easyteam.rocks>
2023-02-04 23:35:13 -0300

    convert gif and video

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { Express } from 'express';
-import { Client, MessageMedia, Message } from 'whatsapp-web.js';
-import { Server }  from 'socket.io'
+import { Client, MessageMedia, Message, MessageTypes } from 'whatsapp-web.js';
+import { Server } from 'socket.io'
 import qrcode from 'qrcode';
 import fs from 'fs';
 
@@ -9,22 +9,34 @@ const app: Express = express();
 const server = app.listen(3000, () => console.log('listening on port 3000'));
 
 const io = new Server(server);
+
+import ffmpegInstaller from "@ffmpeg-installer/ffmpeg";
+const ffprobe = require("@ffprobe-installer/ffprobe");
+
+import ffmpegCreator from "fluent-ffmpeg";
+const ffmpeg = ffmpegCreator();
+
+ffmpeg.setFfprobePath(ffprobe.path)
+    .setFfmpegPath(ffmpegInstaller.path);
+
 const client = new Client({
-	puppeteer: {
-		args: ['--no-sandbox'],
-	}
+    ffmpegPath: ffmpegInstaller.path,
+    puppeteer: {
+        args: ['--no-sandbox'],
+    }
 });
+
 
 let qrCodeImage: string | null = null;
 
 app.get('/', (req, res) => {
     fs.readFile('src/index.html', 'utf-8', (err, data) => {
         if (err) {
-          return res.status(500).send('Erro ao ler arquivo');
+            return res.status(500).send('Erro ao ler arquivo');
         }
-    
+
         return res.send(data);
-      });
+    });
 
     io.emit('message', client.info);
 })
@@ -35,20 +47,22 @@ app.get('/info', (req, res) => {
 
 io.on('connection', (socket) => {
     io.emit('message', 'Connected with socket.');
-
-    client.removeListener('qr', () => {});
-    client.off('qr', () => {});
-    client.on('qr', (qr) => {
-        qrcode.toDataURL(qr, (err, url) => {
-            qrCodeImage = url;
-            io.emit('qr', qrCodeImage);
-            io.emit('message', 'NEW QR Code received, scan please!');
-        });
-    });
-
-    io.emit('qr', qrCodeImage);
-    io.emit('message', 'QR Code received, scan please!');
+    if(!qrCodeImage) {
+        io.emit('message', 'Waiting QR Code...');
+    }
+    else {
+        io.emit('qr', qrCodeImage);
+        io.emit('message', 'QR Code received, scan please!');
+    } 
     console.log('user socket connected');
+});
+
+client.on('qr', (qr) => {
+    qrcode.toDataURL(qr, (err, url) => {
+        qrCodeImage = url;
+        io.emit('qr', qrCodeImage);
+        io.emit('message', 'NEW QR Code received, scan please!');
+    });
 });
 
 client.on('ready', () => {
@@ -65,13 +79,44 @@ client.on('message', msg => {
 client.initialize();
 
 const generateSticker = async (msg: Message) => {
-    if (msg.type === "image") {
-        try {
-            const { data } = await msg.downloadMedia()
-            const image = await new MessageMedia("image/jpeg", data, "image.jpg");
-            msg.reply(image, undefined, { sendMediaAsSticker: true })
-        } catch (e) {
-            msg.reply("❌ Erro ao processar imagem")
+    try {
+        if (msg.type !== MessageTypes.TEXT) {
+            let messageMedia: MessageMedia = await msg.downloadMedia();
+            if (msg.isGif) {
+                const gifBase64 = (<any>msg)._data?.body;
+                messageMedia = new MessageMedia("image/gif", gifBase64, "image.gif");
+            }
+            if(!messageMedia) {
+                msg.reply("❌ Error to process media");
+            }
+            else {
+                msg.reply(messageMedia, undefined, { sendMediaAsSticker: true });
+            }
         }
+
+    } catch (e) {
+        msg.reply("❌ Error to process media");
     }
 }
+/* 
+async function convertGifToMp4(inputBase64: string, onFinished?: (video: string) => void) {
+    const input = `input-${Date.now().toString()}.gif`;
+    const output = `output-${Date.now().toString()}.mp4`;
+    await fs.writeFileSync(input, Buffer.from(inputBase64, "base64"));
+
+    ffmpeg
+        .input(input)
+        .noAudio()
+        .output(output)
+        .on("end", async () => {
+            fs.unlinkSync(input);
+            debugger;
+            let video = (await fs.readFileSync(output)).toString("base64");
+            fs.unlinkSync(output);
+            if (onFinished) onFinished(video);
+            console.log("Finished");
+        })
+        .on("error", (e) => console.log(e))
+        .run();
+} */
+
